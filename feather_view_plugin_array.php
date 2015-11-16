05d3df6eabaa2621185243712dfc08276a2c3794
jsyczhanghao <jsyczhanghao@sina.com>
2015-11-16 20:29:12 +0800

    update array class

--- feather_view_plugin_array.php
+++ feather_view_plugin_array.php
@@ -3,14 +3,18 @@ class Feather_View_Plugin_Array{
 	/*
 	调用数组中的某个key，如果不存在则返回default值，key的格式为a.b.c格式			
 	*/
-	public function get(array $data, $key, $default = null){
-		$keys = explode('.', $key);
+	public function get(array $data, $key = null, $default = null){
+		if($key != null){
+			if(isset($data[$key])) return $data[$key];
 
-		foreach($keys as $key){
-			if(isset($data[$key])){
-				$data = $data[$key];
-			}else{
-				return $default;
+			$keys = explode('.', $key);
+
+			foreach($keys as $key){
+				if(is_array($data) && isset($data[$key])){
+					$data = $data[$key];
+				}else{
+					return $default;
+				}
 			}
 		}
 
